55a48ef16feb3df3211640eb7a37bf67fb73eadc
praveenbalajinayak <113987711+praveenbalajinayak@users.noreply.github.com>
2023-04-05 14:24:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const usersRoutes = require('./routes/users.js')
+const express = require('express');
+const { parse } = require('pg-protocol');
+const app = express();
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+
+const PORT = 9097;
+
+app.use(bodyParser.json());
+
+app.use("/users", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+
+app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
